998d7e80811e4c9a6a868e834f3d1b25e4dd0f50
sushi-chaaaan <mail@sushichan.live>
2024-06-03 13:10:37 +0900

    solve 18

--- src/j6/18.lisp
+++ src/j6/18.lisp
@@ -1,8 +1,7 @@
 (defun depth (lst)
-; >(depth '(1 (2) 3)))
-; 2
-; >(depth '(1 2 (((3)) 4))
-; 4
-;use apply,max,mapcar
-  (if (null lst) 0
-      (1+ (apply #'max (mapcar 'depth lst)))))
+    "Return the depth of the deepest nested list in LST."
+    ;(depth '(1 (2) 3))) => 2
+    ;(depth '(1 2 (((3)) 4)) => 4
+  (if (atom lst)
+      0
+      (1+ (apply #'max (mapcar #'depth lst)))))
