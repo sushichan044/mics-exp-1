38a75d876d2b1f15fe1916b0360fdc5c97861b63
sushi-chaaaan <mail@sushichan.live>
2024-06-12 13:24:29 +0900

    fix ex7

--- src/j4/ex7.c
+++ src/j4/ex7.c
@@ -1,20 +1,16 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <sys/fcntl.h>
 #include <unistd.h>
 
 int main(int argc, char const *argv[]) {
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s <command> <args...> <output destination>\n",
-            argv[0]);
-    return 1;
+  if (argc < 3) {
+    write(2, "Usage: <command> <args...> <output destination>\n", 48);
+    exit(1);
   }
   char const *destination = argv[argc - 1];
   int fd;
   if ((fd = open(destination, O_WRONLY | O_CREAT | O_TRUNC, 0644)) < 0) {
-    perror("error opening destination file: ");
-    perror(destination);
-    perror("\n");
+    write(2, "Error opening destination\n", 26);
     exit(1);
   }
 
@@ -31,5 +27,6 @@ int main(int argc, char const *argv[]) {
   close(fd);
 
   execvp(command, args);
-  return 0;
+
+  exit(0);
 }
